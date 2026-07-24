140934f75fc000c77a247dd61aec808c3f647009
KeysaAnargya <marynkaquinlanyyy@gmail.com>
2026-07-24 21:22:51 +0700

    24110105_Keysa Anargya

---
+++ Persegi1/src/persegi1/Persegi1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package persegi1;
+
+/**
+ *
+ * @author ACER SWIFT 1
+ */
+public class Persegi1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
